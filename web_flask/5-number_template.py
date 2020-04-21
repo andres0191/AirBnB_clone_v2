2b81298c6ea4dc1a2a724686bac9533fc577bfa9
andres0191 <andres_felipe0191@hotmail.com>
2020-04-21 15:46:39 -0500

    modify task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """ Write a script that starts a Flask web application """
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 import sys
 
 app = Flask(__name__)
