14adc0e46774114e76d092f87fbb7fe89abdbf46
andres0191 <andres_felipe0191@hotmail.com>
2020-04-21 15:51:09 -0500

    modify html of task 5

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -34,13 +34,13 @@ def print_python_is_cool(text='is_cool'):
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def print_its_a_number(n):
-    """ display “n is a number” only if n is an integer """
+    """ display "n is a number" only if n is an integer """
     return "%d is a number" % n
 
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def print_number_template(n):
-    """ display “n is a number” only if n is an integer """
+    """ display "n is a number" only if n is an integer """
     return render_template('5-number.html', n=n)
 
 
