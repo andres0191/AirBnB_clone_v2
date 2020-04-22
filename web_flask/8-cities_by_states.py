07efac2299dadc89b8a176d122bbcf21a94ed8a8
andres0191 <andres_felipe0191@hotmail.com>
2020-04-22 13:44:39 -0500

    add task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -10,15 +10,19 @@ import sys
 app = Flask(__name__)
 
 
-
-app = Flask(__name__)
-
-
-@app.route('/cities_list', strict_slashes=False)
+@app.route('/states_list', strict_slashes=False)
 def print_states_list():
     """ display a HTML page: (inside the tag BODY) """
-    list_of_cities = storage.all('Citie')
-    return render_template('8-cities_by_states.html', list_of_cities=list_of_cities)
+    list_of_state = storage.all('State')
+    return render_template('7-states_list.html', list_of_state=list_of_state)
+
+
+@app.teardown_appcontext
+def close_storage(self):
+    """ Declare a method to handle
+        @app.teardown_appcontext
+    """
+    storage.close()
 
 
 if __name__ == '__main__':
