9b7a06cf478089f28332f701b293fb3f9bd45dd2
andres0191 <andres_felipe0191@hotmail.com>
2020-04-21 21:35:44 -0500

    modify task 8

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,15 +10,17 @@ app = Flask(__name__)
 
 
 @app.route('/states_list', strict_slashes=False)
-def states_list():
-    """gets states lists"""
-    state_list = storage.all('State')
-    return render_template('7-states_list.html', state_list=state_list)
+def print_states_list():
+    """ display a HTML page: (inside the tag BODY) """
+    list_of_state = storage.all('State')
+    return render_template('7-states_list.html', list_of_state=list_of_state)
 
 
-@app.teardown_appcontext
-def tear_down_db(n):
-    """ sql session closes """
+@app.teardown_appcontext()
+def close_storage():
+    """ Declare a method to handle
+        @app.teardown_appcontext
+    """
     storage.close()
 
 
