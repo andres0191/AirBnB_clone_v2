7113a10d9bda14b786d48994452b38a46ff64467
andres0191 <andres_felipe0191@hotmail.com>
2020-04-22 14:16:09 -0500

    modify task 8

--- web_flask/8-cities_by_states.py
+++ web_flask/8-cities_by_states.py
@@ -1,3 +1,30 @@
 #!/usr/bin/python3
 """ Write a script that starts a Flask web application """
 from flask import Flask
+from flask import render_template
+from models import storage
+from models.state import State
+import sys
+
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def print_states_list():
+    """ display a HTML page: (inside the tag BODY) """
+    list_of_cities = storage.all('Citie')
+    return render_template('8-cities_by_states.html', list_of_cities=list_of_cities)
+
+
+@app.teardown_appcontext
+def close_storage(self):
+    """ Declare a method to handle
+        @app.teardown_appcontext
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    """ Web app must be listening on 0.0.0.0 port 5000 """
+    app.run(host='0.0.0.0', port=5000)
