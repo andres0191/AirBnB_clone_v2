92ffe218cdc4c5ed5fc41e1636e73c9f269b8bec
andres0191 <andres_felipe0191@hotmail.com>
2020-05-25 15:28:17 +0000

    complete task 9

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,35 +1,48 @@
 #!/usr/bin/python3
-""" Write a script that starts a Flask web application """
-from flask import Flask
-from flask import render_template
+""" Starts a Flash Web Application """
 from models import storage
 from models.state import State
-import sys
-
-
+from os import environ
+from flask import Flask, render_template
 app = Flask(__name__)
-
-
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<int:id>', strict_slashes=False)
-def states_id(id=None):
-    """ display a HTML page: (inside the tag BODY) """
-    if id is not None:
-        id = 'State.'+id
-    cities = storage.all('City')
-    dict_states = storage.all('State')
-    return render_template('9-states.html',
-                           dict_states=dict_states, id=id, cities=cities)
+# app.jinja_env.trim_blocks = True
+# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def close_storage(self):
-    """ Declare a method to handle
-        @app.teardown_appcontext
-    """
+def close_db(error):
+    """ Remove the current SQLAlchemy Session """
     storage.close()
 
 
-if __name__ == '__main__':
-    """ Web app must be listening on 0.0.0.0 port 5000 """
+@app.route('/states', strict_slashes=False)
+@app.route('/states/<id>', strict_slashes=False)
+def states_state(id=""):
+    """ displays a HTML page with a list of cities by states """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    found = 0
+    state = ""
+    cities = []
+
+    for i in states:
+        if id == i.id:
+            state = i
+            found = 1
+            break
+    if found:
+        states = sorted(state.cities, key=lambda k: k.name)
+        state = state.name
+
+    if id and not found:
+        found = 2
+
+    return render_template('9-states.html',
+                           state=state,
+                           array=states,
+                           found=found)
+
+
+if __name__ == "__main__":
+    """ Main Function """
     app.run(host='0.0.0.0', port=5000)
