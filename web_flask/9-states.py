59c6e23f9914b00538f38af9cf8cbf65c855a335
andres0191 <andres_felipe0191@hotmail.com>
2020-05-25 15:51:51 +0000

    update task 10

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -1,48 +1,43 @@
 #!/usr/bin/python3
-""" Starts a Flash Web Application """
+"""First Task"""
+from flask import Flask
+from flask import render_template
 from models import storage
 from models.state import State
-from os import environ
-from flask import Flask, render_template
+
 app = Flask(__name__)
-# app.jinja_env.trim_blocks = True
-# app.jinja_env.lstrip_blocks = True
 
 
 @app.teardown_appcontext
-def close_db(error):
-    """ Remove the current SQLAlchemy Session """
+def close(error):
     storage.close()
 
 
-@app.route('/states', strict_slashes=False)
-@app.route('/states/<id>', strict_slashes=False)
-def states_state(id=""):
-    """ displays a HTML page with a list of cities by states """
-    states = storage.all(State).values()
-    states = sorted(states, key=lambda k: k.name)
-    found = 0
-    state = ""
-    cities = []
+@app.route("/states", strict_slashes=False)
+def task9():
+    return render_template(
+            "9-states.html",
+            state=storage.all(State),
+            t="v")
 
-    for i in states:
-        if id == i.id:
-            state = i
-            found = 1
-            break
-    if found:
-        states = sorted(state.cities, key=lambda k: k.name)
-        state = state.name
 
-    if id and not found:
-        found = 2
-
-    return render_template('9-states.html',
-                           state=state,
-                           array=states,
-                           found=found)
+@app.route("/states/<id>", strict_slashes=False)
+def task9o(id):
+    objr = None
+    obj = storage.all(State)
+    for k, v in obj.items():
+        if v.id == id:
+            objr = v
+    if objr is None:
+        return render_template(
+                "9-states.html",
+                t="n",
+                msg="Not found!")
+    return render_template(
+            "/9-states.html",
+            state=objr,
+            t="u")
 
 
 if __name__ == "__main__":
-    """ Main Function """
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host="0.0.0.0", port=5000)
